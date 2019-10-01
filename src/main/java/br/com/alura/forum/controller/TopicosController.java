5c070409f3a39405263b561efb4dc3a62035b4b9
Samuel Alves <samuel241296@hotmail.com>
2019-09-30 21:19:04 -0300

    aula 02 completa

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -7,6 +7,8 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
@@ -41,6 +43,7 @@ public class TopicosController {
 	private CursoRepository cursoReposity;
 
 	@GetMapping
+	@Cacheable(value="listaDeTopicos")
 	public Page<TopicoDto> topicos(@RequestParam(required = false) String nomeCurso,
 			@PageableDefault(sort="id", direction = Direction.ASC, page=0,size=10) Pageable paginacao) {
 		
@@ -57,6 +60,7 @@ public class TopicosController {
 
 	@PostMapping
 	@Transactional
+	@CacheEvict(value="listaDeTopicos",allEntries = true)
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 
 		Topico topico = form.converter(cursoReposity);
@@ -80,6 +84,7 @@ public class TopicosController {
 
 	@PutMapping("/{id}")
 	@Transactional
+	@CacheEvict(value="listaDeTopicos",allEntries = true)
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
 		Optional<Topico> optional = topicoReposity.findById(id);
 		if (optional.isPresent()) {
@@ -92,6 +97,7 @@ public class TopicosController {
 
 	@DeleteMapping("/{id}")
 	@Transactional
+	@CacheEvict(value="listaDeTopicos",allEntries = true)
 	public ResponseEntity<?> remover(@PathVariable Long id) {
 		Optional<Topico> topico = topicoReposity.findById(id);
 		if (topico.isPresent()) {
