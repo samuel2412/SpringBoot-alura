22e6206fdffe029ac8f2beae7e8b1f45a0412efa
Samuel Alves <samuel241296@hotmail.com>
2019-10-02 16:04:34 -0300

    aula 07 concluida

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -60,8 +60,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	//gerencia recursos estaticos (js, css, imagens, etc..)
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		
+	    web.ignoring()
+	        .antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
 	}
-
 	
 }
